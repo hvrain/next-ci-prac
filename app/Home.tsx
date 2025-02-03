7de6d95592182b3c891dfa33ce969c40672a6658
hvrain <hvrain3222@naver.com>
2025-02-03 15:34:27 +0900

    Feat: workflows init

---
+++ app/Home.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+	)
+}
